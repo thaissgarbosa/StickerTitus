dc6edd25cc40c79529cdec00bbcb9dbf5f0e04c2
thaissgarbosa <thais.ssgarbosa@gmail.com>
2025-02-12 11:44:17 -0300

    chose: create and apply differents buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,17 +2,17 @@ import { View, StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
 
-const PlaceholderImage = require("@/assets/images/background-image.jpg")
+const PlaceholderImage = require("@/assets/images/background-image.jpg");
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
-        <View style={styles.footerContainer}>
-          <Button label= "Choose a photo"/>
-          <Button label= "Use this photo"/>
-        </View>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme="primary" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -29,8 +29,8 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 28,
   },
-  footerContainer:{
-    flex: 1/3,
+  footerContainer: {
+    flex: 1 / 3,
     alignItems: "center",
   },
 });
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,38 +1,72 @@
+import { FontAwesome } from "@expo/vector-icons";
 import { StyleSheet, View, Pressable, Text } from "react-native";
 
 type Props = {
-    label: string;
+  label: string;
+  theme?: "primary"; // ? para sinalizar que a props é opcional
 };
 
-export default function Button({label}:Props){
+export default function Button({ label, theme }: Props) {
+  if (theme === "primary") {
     return (
-        <View style={styles.buttonContainer}>
-            <Pressable  style={styles.button} onPress={()=> alert("Você clicou aqui!")}>
-                <Text style={styles.buttonLabel}> {label}</Text>
-            </Pressable>
-        </View>
+      <View
+        style={[
+          styles.buttonContainer,
+          { borderWidth: 4, borderColor: "#FFD33D", borderRadius: 18 },
+        ]}
+      >
+        <Pressable
+          style={[styles.button, { backgroundColor: "#FFF" }]}
+          onPress={() => alert("Você clicou aqui!")}
+        >
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color={"25292E"}
+            style={styles.buttonIcon}
+          />
+          <Text style={[styles.buttonLabel, { color: "#25292E" }]}>
+            {" "}
+            {label}
+          </Text>
+        </Pressable>
+      </View>
     );
+  }
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable
+        style={styles.button}
+        onPress={() => alert("Você clicou aqui!")}
+      >
+        <Text style={styles.buttonLabel}> {label}</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    buttonContainer:{
-        width: 320,
-        height: 68,
-        marginHorizontal: 20,
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 3
-    },
-    button:{
-        borderRadius: 10,
-        width: "100%",
-        height: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "row",
-    },
-    buttonLabel:{
-        color: "#FFF",
-        fontSize: 16,
-    },
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 3,
+  },
+  button: {
+    borderRadius: 10,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  buttonLabel: {
+    color: "#FFF",
+    fontSize: 16,
+  },
+  buttonIcon:{
+    paddingRight: 8,
+  },
 });
